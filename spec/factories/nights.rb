56820e0aae15b91526dc153e1ff2c73f5b381ddf
sorasora313 <sky.313.sora@gmail.com>
2022-08-23 16:41:35 +0900

    テスト実行

--- spec/factories/nights.rb
+++ spec/factories/nights.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :night do
-    
+    association :user
+
+    after(:build) do |night|
+      night.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+    end
+
+    title {"海"}
+    explain {"すごい"}
+    genre_id {3}
+    state_id {3}
+
   end
 end
