cef73261f58a19e87deb72d3203d5ab9e446ae44
sorasora313 <sky.313.sora@gmail.com>
2022-08-24 06:01:35 +0900

    ページ遷移の制限

--- app/controllers/nights_controller.rb
+++ app/controllers/nights_controller.rb
@@ -1,5 +1,7 @@
 class NightsController < ApplicationController
-
+  before_action :set_nights, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   def index
     @night = Night.all.order("created_at DESC")
   end
@@ -50,6 +52,13 @@ class NightsController < ApplicationController
   private
   def night_params
     params.require(:night).permit(:image, :title, :explain, :genre_id,:state_id).merge(user_id: current_user.id)
+  end
+  def set_nights
+    @night = Night.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @night.user
   end
   
 
