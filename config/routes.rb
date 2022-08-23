6eb676e8f8e26dd7d46e3824427550e99b40d739
sorasora313 <sky.313.sora@gmail.com>
2022-08-23 22:19:55 +0900

    詳細ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "nights#index"
+  resources :users, only: [:show]
   resources :nights do
     resources :comments, only: [:create]
   end
