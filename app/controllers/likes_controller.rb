9ef4d50642eb7c4d30137a4e12e7217996b71859
sorasora313 <sky.313.sora@gmail.com>
2022-08-27 12:40:19 +0900

    いいね昨日の改善

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,13 +4,13 @@ class LikesController < ApplicationController
     night = Night.find(params[:night_id])
     like = current_user.likes.new(night_id: night.id)
     like.save
-    redirect_to nights_path
+    redirect_to night_path(like.night)
   end
 
   def destroy
     night = Night.find(params[:night_id])
     like = current_user.likes.find_by(night_id: night.id)
     like.destroy
-    redirect_to nights_path
+    redirect_to night_path(like.night)
   end
 end
